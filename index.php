a8cbec7d6e686e575f2acc0025cace3ecbf7356a
Stiven <codestiven@gmail.com>
2024-03-17 18:08:16 -0400

    Corregir errores de estilo en la interfaz de usuario

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Archivo: index.php
+
+echo "<html>";
+echo "<head>";
+echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+echo "</head>";
+echo "<body>";
+
+// Botón original
+echo "<button class='button'>Click me</button>";
+
+// Nueva versión mejorada del botón
+echo "<button class='button improved-button'>Click me (Improved)</button>";
+
+echo "</body>";
+echo "</html>";
+?>
